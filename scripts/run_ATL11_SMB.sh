42ffc02df046c333891f2a2a653495f1fcd8b4dc
Ben Smith <bsmith@apl.washington.edu>
2020-09-25 08:34:07 -0600

    scripts/run_ATL11_SMB.sh : script to automate ATL11 updates

---
+++ scripts/run_ATL11_SMB.sh
@@ -0,0 +1,11 @@
+#append_SMB_ATL11.py --directory=/Volumes/ice1/tyler/ --model=MAR,RACMO,MERRA2-hybrid --region=GL /Volumes/ice1/tyler/rel007_dump_every_4th.h5
+append_SMB_ATL11.py --directory=/Volumes/ice1/tyler/ --model=MAR,RACMO,MERRA2-hybrid --region=GL /Volumes/ice1/tyler/007_crossover_data_v1.h5
+​
+#append_SMB_averages_ATL11.py --directory=/Volumes/ice1/tyler/ --model=MAR --region=GL --year=2000,2019 /Volumes/ice1/tyler/rel007_dump_every_4th.h5
+append_SMB_averages_ATL11.py --directory=/Volumes/ice1/tyler/ --model=MAR --region=GL --year=2000,2019 /Volumes/ice1/tyler/007_crossover_data_v1.h5
+​
+#append_SMB_mean_ATL11.py --directory=/Volumes/ice1/tyler/ --model=MAR --region=GL --year=1980,1996 /Volumes/ice1/tyler/rel007_dump_every_4th.h5
+append_SMB_mean_ATL11.py --directory=/Volumes/ice1/tyler/ --model=MAR --region=GL --year=1980,1996 /Volumes/ice1/tyler/007_crossover_data_v1.h5
+​
+#append_SMB_mean_ATL11.py --directory=/Volumes/ice1/tyler/ --model=MAR --region=GL --year=2000,2019 /Volumes/ice1/tyler/rel007_dump_every_4th.h5
+#append_SMB_mean_ATL11.py --directory=/Volumes/ice1/tyler/ --model=MAR --region=GL --year=2000,2019 /Volumes/ice1/tyler/007_crossover_data_v1.h5
