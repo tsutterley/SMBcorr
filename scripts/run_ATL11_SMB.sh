708f76ba373f7ef097a7b806e9e10e49c325f8c9
tsutterley <tsutterl@uw.edu>
2021-02-18 10:13:56 -0800

    update MERRA-2 hybrid programs for grids
    in append: set keyword argument dict with standard and optional parameters
    
    update append ATL11 notebooks
    
    update run shell

--- scripts/run_ATL11_SMB.sh
+++ scripts/run_ATL11_SMB.sh
@@ -1,25 +1,44 @@
+#!/bin/bash
 
-dest_dir=/Volumes/ice2/ben/MAR/ATL11_with_corrections/temp
-#xover_file=$dest_dir/test_xovers.h5
+# dest_dir=/Volumes/ice2/ben/MAR/ATL11_with_corrections/temp
+dest_dir=/Volumes/ice3/tyler/MAR/ATL11_with_corrections/rel002a
 
-xover_file=$dest_dir/007_crossover_data_v1.h5
-AT_file=$dest_dir/rel007_dump_every_4th.h5
+atl11_files=( "rel002_crossover_data.h5" "rel002_dump_every_2nd.h5" )
 
-echo "Crossover_file: "$xover_file
-echo "Along-track file: "$AT_file
-#exit
+for i in "${atl11_files[@]}"
+do
 
-echo "SMB"
-append_SMB_ATL11.py --directory /Volumes/ice1/tyler/ --model MAR RACMO MERRA2-hybrid --region GL $AT_file
-append_SMB_ATL11.py --directory /Volumes/ice1/tyler/ --model MAR RACMO MERRA2-hybrid --region GL $xover_file
+    # check if running crossover or along track file
+    if [[ $i =~ "crossover" ]]
+    then
+        j="Crossover"
+    else
+        j="Along-track"
+    fi
 
-#echo "SMB averages"
-#append_SMB_averages_ATL11.py --directory /Volumes/ice1/tyler/ --model MAR --region GL --year 2000,2019 $AT_file
-#append_SMB_averages_ATL11.py --directory /Volumes/ice1/tyler/ --model MAR --region GL --year 2000,2019 $xover_file
+    # test if file exists
+    atl11_file="$dest_dir/$i"
+    if [ -f "$atl11_file" ]
+    then
+        printf "ATL11 %s File: %s\n" $j $atl11_file
+    else
+        printf "ATL11 %s File %s not found!\n" $j $atl11_file
+        continue
+    fi
 
-echo "SMB mean"
-append_SMB_mean_ATL11.py --directory /Volumes/ice1/tyler/ --model MAR --region GL --year 1980,1995 $AT_file
-append_SMB_mean_ATL11.py --directory /Volumes/ice1/tyler/ --model MAR --region GL --year 1980,1995 $xover_file
+    echo "SMB"
+    append_SMB_ATL11.py --directory /Volumes/ice3/tyler/ \
+        --model MERRA2-hybrid --region GL $atl11_file
 
-#append_SMB_mean_ATL11.py --directory /Volumes/ice1/tyler/ --model MAR --region GL --year 2000,2019 $AT_file
-#append_SMB_mean_ATL11.py --directory /Volumes/ice1/tyler/ --model MAR --region GL --year 2000,2019 $xover_file
+    # # echo "SMB averages"
+    # # append_SMB_averages_ATL11.py --directory /Volumes/ice3/tyler/ \
+    # #     --model MAR --region GL --year 2000 2019 $atl11_file
+
+    echo "SMB mean"
+    append_SMB_mean_ATL11.py --directory /Volumes/ice3/tyler/ \
+       --model MAR --region GL --year 1980 1995 $atl11_file
+
+    # # append_SMB_mean_ATL11.py --directory /Volumes/ice3/tyler/ \
+    # #     --model MAR --region GL --year 2000 2019 $atl11_file
+
+done
