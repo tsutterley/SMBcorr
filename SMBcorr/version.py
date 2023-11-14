32088096be48e46e45dd2649a038f732570dac8d
Tyler Sutterley <tsutterl@uw.edu>
2023-11-14 13:25:45 -0800

    fix: replace deprecated pkg_resources with importlib (#22)
    ci: bump versions to prevent deprecation errors

--- SMBcorr/version.py
+++ SMBcorr/version.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 u"""
-version.py (04/2021)
+version.py (11/2023)
 Gets version number of a package
 """
-from pkg_resources import get_distribution
+import importlib.metadata
 
+# package metadata
+metadata = importlib.metadata.metadata("SMBcorr")
 # get version
-version = get_distribution("SMBcorr").version
+version = metadata["version"]
 # append "v" before the version
 full_version = "v{0}".format(version)
 # get project name
-project_name = get_distribution("SMBcorr").project_name
+project_name = metadata["Name"]
